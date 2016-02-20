4b36b0e5dec6083b0f3dbbf4d2d321d0d1b1be08
isnu <isnu.din@gmail.com>
2016-02-20 07:07:54 +0700

    Revert "aplikasi-pelatihan"
    This reverts commit 8cbb7284dba9a8be92c71ddae5fe3dba04bc9be3.

--- src/main/java/com/ajariaku/pelatihan/dao/MateriDao.java
+++
@@ -1,8 +0,0 @@
-package com.ajariaku.pelatihan.dao;
-
-import com.ajariaku.pelatihan.entity.Materi;
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface MateriDao extends PagingAndSortingRepository<Materi, String>{
-    
-}
